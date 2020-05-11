a5f06b640b5c9f826b312555a5041292d37ba675
Dan S <63663822+Phoenka@users.noreply.github.com>
2020-05-11 17:52:21 +0200

    .

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// NAVBAR FIRST OPTION LEFT BORDER
+$("#navigationBarList li a").first().css("border-left", "1px solid white");
+
+// NAVBAR SLIDE OPTIONS
 $("#navigationBarList li").hover(function(){
     $(this).children("ul").stop(true, false, true).slideToggle(300)
+});
+
+// FIRST TAB SHOWS ON LOAD
+$(".mainContainer").first().addClass("active");
+
+// CLICKING NAVIGATION OPTIONS
+$("#navigationBarList li a").on("click", function(){
+    $(".mainContainer").removeClass("active");
+    alert($("#navigationBarList li a").index(this));
+    $(".mainContainer").eq($("#navigationBarList li a").index(this)).addClass("active");
 });
