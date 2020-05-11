ac0d595d1789773bd889153f71e0e231e45110d1
Dan S <63663822+Phoenka@users.noreply.github.com>
2020-05-11 22:55:34 +0200

    .

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $("#navigationBarList li a").first().css("border-left", "1px solid white");
 
 // DECIDING WHETHER TO USE CURRENT LEARNIGN + RANDOM QUOTE
 $(window).on("load resize", function(){
-    if ($(window).width() < 1000){
+    if ($(window).width() < 970){
         $("#randomQuote, #currentlyLearning").css("display", "none");
 }   else {
         $("#randomQuote, #currentlyLearning").css("display", "inline");
@@ -26,22 +26,49 @@ $("#navigationBarList li a").on("click", function(){
 });
 
 // DOWNLOAD CV DEFAULT SELECTOR
-// $("#selectCV div").filter(":eq(0), :eq(2)").addClass("CVselected");
-
 $("#selectCV div").filter(":eq(0), :eq(2)").addClass("CVselected");
+
+
 
 // DOWNLOAD CV SELECT
 // DOESN'T MAKE SENSE WHY THIS DOES NOT WORK :()
+// $("#selectCV div").on("click", function(){
+//     if ($(this).hasClass("CVselected")){
+//         alert("error");
+//     } else if ($(this).index() < 2) {
+//         $("#selectCV div").eq(0).toggleClass("CVselected");
+//         $("#selectCV div").eq(1).toggleClass("CVSelected");
+//     } else {
+//         $("#selectCV div").eq(2).toggleClass("CVSelected");
+//         $("#selectCV div").eq(3).toggleClass("CVSelected");
+//     }
+// })
+
+// DOWNLOAD CV SELECT
 $("#selectCV div").on("click", function(){
-    if ($(this).hasClass("CVselected")){
-        alert("error");
-    } else if ($(this).index() < 2) {
+    if ($(this).hasClass("CVselected")) {
+    }   else if ($(this).index() == 0) {
+        $(this).toggleClass("CVselected");
+        $("#selectCV div").eq(1).toggleClass("CVselected");
+    }   else if ($(this).index() == 1) {
+        $(this).toggleClass("CVselected");
         $("#selectCV div").eq(0).toggleClass("CVselected");
-        $("#selectCV div").eq(1).toggleClass("CVSelected");
-    } else {
-        $("#selectCV div").eq(2).toggleClass("CVSelected");
-        $("#selectCV div").eq(3).toggleClass("CVSelected");
+    }   else if ($(this).index() == 2) {
+        $(this).toggleClass("CVselected");
+        $("#selectCV div").eq(3).toggleClass("CVselected");
+    }   else if ($(this).index() == 3) {
+        $(this).toggleClass("CVselected");
+        $("#selectCV div").eq(2).toggleClass("CVselected");
     }
-})
+    if ($("#selectCV div").eq(0).hasClass("CVselected") && $("#selectCV div").eq(2).hasClass("CVselected")){
+        $("#linkToCV").attr("href", "https://drive.google.com/open?id=1O6i0fCbxdvZg0ZimI4Ok84tAS1nZHaCs");
+    }   else if ($("#selectCV div").eq(1).hasClass("CVselected") && $("#selectCV div").eq(2).hasClass("CVselected")){
+        $("#linkToCV").attr("href", "https://drive.google.com/open?id=1fH3PKscd9yETH554VBbULMvGtjcpVoxK");
+    }   else if ($("#selectCV div").eq(0).hasClass("CVselected") && $("#selectCV div").eq(3).hasClass("CVselected")){
+        $("#linkToCV").attr("href", "https://drive.google.com/open?id=1D8teIo29ZaGMQHtmEPh1goIW4WMpiz9u");
+    }   else {
+        $("#linkToCV").attr("href", "https://drive.google.com/open?id=1m2Ro_QAXTqfrSieI3THVH-HsTAA_M4Vf");
+    }
+});
 
 // SOLVE MARGIN WHEN SCREEN IS SMALLER
