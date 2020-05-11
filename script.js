0d3e3c9c1856522ab9ba6a0cb374639c8f79cae2
Dan S <63663822+Phoenka@users.noreply.github.com>
2020-05-11 21:54:55 +0200

    .

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // NAVBAR FIRST OPTION LEFT BORDER
 $("#navigationBarList li a").first().css("border-left", "1px solid white");
 
+// DECIDING WHETHER TO USE CURRENT LEARNIGN + RANDOM QUOTE
+$(window).on("load resize", function(){
+    if ($(window).width() < 1000){
+        $("#randomQuote, #currentlyLearning").css("display", "none");
+}   else {
+        $("#randomQuote, #currentlyLearning").css("display", "inline");
+}
+});
+
 // NAVBAR SLIDE OPTIONS
 $("#navigationBarList li").hover(function(){
     $(this).children("ul").stop(true, false, true).slideToggle(300)
@@ -12,6 +21,27 @@ $(".mainContainer").first().addClass("active");
 // CLICKING NAVIGATION OPTIONS
 $("#navigationBarList li a").on("click", function(){
     $(".mainContainer").removeClass("active");
-    alert($("#navigationBarList li a").index(this));
+    // FIGURE OUT WHY INSTEAD OF eq($("#navigationBarList li a").index(this)) THIS CANNOT BE USED eq($(this).index())
     $(".mainContainer").eq($("#navigationBarList li a").index(this)).addClass("active");
 });
+
+// DOWNLOAD CV DEFAULT SELECTOR
+// $("#selectCV div").filter(":eq(0), :eq(2)").addClass("CVselected");
+
+$("#selectCV div").filter(":eq(0), :eq(2)").addClass("CVselected");
+
+// DOWNLOAD CV SELECT
+// DOESN'T MAKE SENSE WHY THIS DOES NOT WORK :()
+$("#selectCV div").on("click", function(){
+    if ($(this).hasClass("CVselected")){
+        alert("error");
+    } else if ($(this).index() < 2) {
+        $("#selectCV div").eq(0).toggleClass("CVselected");
+        $("#selectCV div").eq(1).toggleClass("CVSelected");
+    } else {
+        $("#selectCV div").eq(2).toggleClass("CVSelected");
+        $("#selectCV div").eq(3).toggleClass("CVSelected");
+    }
+})
+
+// SOLVE MARGIN WHEN SCREEN IS SMALLER
